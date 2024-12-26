a8f6deeef637a8306d7687433621bf18f537cc4f
Kostya_B <beztalannyjkonstantin@gmail.com>
2024-12-26 13:55:11 +0200

    Внес изменения

--- text.py
+++ text.py
@@ -1,19 +1,19 @@
 from tkinter import *
 
 
-class Inteface:
+class Interface:
     def __init__(self, main):
         self.file = None
-        self.f = Frame(main)
-        self.f_b = Frame(main)
-        self.entry = Entry(self.f, width=50, bg="grey", fg="yellow")
+        self.frame_top = Frame(main)
+        self.f_bottom = Frame(main)
+        self.entry = Entry(self.frame_top, width=50, bg="grey", fg="yellow")
         self.button_open = Button(
-            self.f, width=20, bg="black", fg="yellow", text="Open"
+            self.frame_top, width=20, bg="black", fg="yellow", text="Open"
         )
         self.button_save = Button(
-            self.f, width=20, bg="black", fg="yellow", text="Save"
+            self.frame_top, width=20, bg="black", fg="yellow", text="Save"
         )
-        self.text = Text(self.f_b, width=75, height=35, bg="darkgreen", fg="white")
+        self.text = Text(self.f_bottom, width=75, height=35, bg="darkgreen", fg="white")
         self.scroll_y = Scrollbar(command=self.text.yview)
         self.scroll_x = Scrollbar(command=self.text.xview, orient=HORIZONTAL)
 
@@ -24,20 +24,24 @@ class Inteface:
         self.button_save["command"] = self.save_info
 
         self.scroll_y.pack(side=RIGHT, fill=Y)
-        self.scroll_x.pack(side=BOTTOM, fil=X)
-        self.f.pack()
-        self.f_b.pack()
+        self.scroll_x.pack(side=BOTTOM, fill=X)
+        self.frame_top.pack()
+        self.f_bottom.pack()
         self.entry.pack(side=LEFT, fill=Y)
         self.button_open.pack(side=LEFT)
         self.button_save.pack(side=LEFT)
         self.text.pack(side=BOTTOM)
 
     def open_info(self):
-        self.text.delete(1.0, END)
-        s = open(self.entry.get(), "r+")
+        try:
+            self.text.delete(1.0, END)
+            s = open(self.entry.get(), "r+")
 
-        self.text.insert(1.0, s.read())
-        self.file = s
+            self.text.insert(1.0, s.read())
+            self.file = s
+            self.save_info()
+        except FileNotFoundError as e:
+            self.text.insert(1.0, f"\nError, file not find")
 
     def save_info(self):
         x = self.text.get(1.0, END)
@@ -51,7 +55,7 @@ class Inteface:
 
 root = Tk()
 
-i = Inteface(root)
+i = Interface(root)
 root["bg"] = "purple"
 
 root.mainloop()
