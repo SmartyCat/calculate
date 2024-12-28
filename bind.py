c8bfcdbc65d7f0a907912e9f025e3c5804b900db
Kostya_B <beztalannyjkonstantin@gmail.com>
2024-12-28 17:39:03 +0200

    update

--- bind.py
+++ bind.py
@@ -3,19 +3,24 @@ from tkinter import *
 
 class Inteface:
     def __init__(self):
+
         self.entry = Entry(width=20, bg="purple", fg="yellow")
         self.list = Listbox(bg="darkblue", fg="yellow")
+        self.sroll = Scrollbar(command=self.list.yview)
 
         self.entry.bind("<Return>", self.go_to_list)
         self.list.bind("<Double-Button-1>", self.go_to_entry)
+        self.list.config(yscrollcommand=self.sroll.set)
 
+        self.sroll.pack(side=RIGHT, fill=Y)
         self.entry.pack()
         self.list.pack()
 
     def go_to_list(self, info):
         x = self.entry.get().split()
-        for i in x:
-            self.list.insert(END, i)
+        if x:
+            for i in x:
+                self.list.insert(END, i)
         self.entry.delete(0, END)
 
     def go_to_entry(self, info):
