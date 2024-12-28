e8af160f2fff153e1baa5d39b1860d971689b5e8
Kostya_B <beztalannyjkonstantin@gmail.com>
2024-12-28 17:21:22 +0200

    bind project

--- bind.py
+++ bind.py
@@ -1,17 +1,30 @@
 from tkinter import *
 
+
+class Inteface:
+    def __init__(self):
+        self.entry = Entry(width=20, bg="purple", fg="yellow")
+        self.list = Listbox(bg="darkblue", fg="yellow")
+
+        self.entry.bind("<Return>", self.go_to_list)
+        self.list.bind("<Double-Button-1>", self.go_to_entry)
+
+        self.entry.pack()
+        self.list.pack()
+
+    def go_to_list(self, info):
+        x = self.entry.get().split()
+        for i in x:
+            self.list.insert(END, i)
+        self.entry.delete(0, END)
+
+    def go_to_entry(self, info):
+        select = self.list.curselection()
+        self.entry.insert(END, self.list.get(select) + " ")
+        self.list.delete(select)
+
+
 root = Tk()
-
-
-def change(event):
-    b["fg"] = "red"
-    b["activeforeground"] = "purple"
-
-
-b = Button(text="Red", width=10, height=3)
-b.bind("<Button-1>", change)
-b.bind("<Return>", change)
-
-b.pack()
+i = Inteface()
 
 root.mainloop()
